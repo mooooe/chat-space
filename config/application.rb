8dcbc57974029a385523228275982bedfe9b5a63
高取もえ <aus.leon725@gmail.com>
2016-11-13 13:13:41 +0900

    set the generator in application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,11 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+    g.helper false
+    g.test_framework false
+    g.coffee false
+    g.assets false
+    g.yaml false
   end
 end
