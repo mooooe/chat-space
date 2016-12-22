cab49dfcd3a2af9d5afc60bb2f8d556a97eea6a3
高取もえ <aus.leon725@gmail.com>
2016-12-23 02:30:26 +0900

    user faker in factorygirl

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
 
   factory :user do
-    name                  "moe"
-    email                 "aaa@gmail.com"
-    password              "00000000"
-    password_confirmation "00000000"
+    name                  Faker::Name.first_name
+    email                 Faker::Internet.email
+    password              Faker::Internet.password(8)
+    password_confirmation Faker::Internet.password(8)
   end
 
 end
