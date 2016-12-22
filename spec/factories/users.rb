fa68257029d57f9d7cd4e0d0db0413a31df163e1
高取もえ <aus.leon725@gmail.com>
2016-12-23 00:11:41 +0900

    use spec to test user model and define it in factorygirl

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+  factory :user do
+    name                  "moe"
+    email                 "aaa@gmail.com"
+    password              "00000000"
+    password_confirmation "00000000"
+  end
+
+end
